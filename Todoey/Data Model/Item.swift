b4fa0f0d6761b13bd2024e06fa8ceb7beb5b1d2b
ivan-blinov-exa <ivanblinov@exabeam.com>
2019-08-26 13:19:12 +0300

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ivan Blinov on 8/26/19.
+//  Copyright © 2019 Ivan Blinov. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+}
